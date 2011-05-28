be6cdb7af3fc10c2f8cc704e94e13f109ee5efba
Luis Sánchez Gómez <qwertylinuxdeb@gmail.com>
2011-05-28 11:32:06 +0200

    Fix message indicating that "firefox4" symbolic link already exits. Always delete it.

--- in/firefox4-acd.mod/script.sh
+++ in/firefox4-acd.mod/script.sh
@@ -30,11 +30,11 @@ echo "Detected architecture is $ARCH"
 wget -v -r -np http://releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-$ARCH/es-ES/ --directory-prefix="/tmp"
 tar -xvf /tmp/releases.mozilla.org/pub/mozilla.org/firefox/releases/latest/linux-$ARCH/es-ES/firefox-*.tar.bz2 -C /usr/local/lib
 if [ -e /usr/local/bin/firefox4 ]; then
-	echo "File named firefox4 already exits. Erase it? (y/s [yes] recommended)"
+	echo "File named firefox4 already exits, deleting..."
 	rm -rfv /usr/local/bin/firefox4
 fi
 
-# Probably download of Firefox4 wasn't successfully  
+# Probably download of Firefox4 wasn't successfully
 if [ -e /usr/local/lib/firefox/firefox ]; then
 	ln -s /usr/local/lib/firefox/firefox /usr/local/bin/firefox4
 else
