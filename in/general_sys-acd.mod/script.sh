a20e3787a7b1cfbfc650bf28f804001ef6446682
Luis Sánchez Gómez <qwertylinuxdeb@gmail.com>
2011-05-30 17:18:34 +0200

    Add some files to install [general_sys]
    Files are /etc/profile and /etc/security/limits.conf

--- in/general_sys-acd.mod/script.sh
+++ in/general_sys-acd.mod/script.sh
@@ -22,7 +22,9 @@ TIME_ID_BACKUP="`date +%d_%m_%Y_%k_%M_%N`"
 
 files_tosetup=(	"/etc/default/rcS"
 		"/etc/profile"
-		"/etc/inittab")
+		"/etc/inittab"
+		"/etc/proalias"
+		"/etc/security/limits.conf")
 
 echo "${#files_tosetup[*]} files are going to be installed:"
 echo "${files_tosetup[*]}"
@@ -31,7 +33,6 @@ for file_path in ${files_tosetup[*]}; do
 	if [ -e "$file_path" ]; then
 		cp  --preserve=mode,timestamps -vr "$file_path" "`dirname $file_path`/.`basename $file_path`.$TIME_ID_BACKUP"
 		echo "File $file_path saved at `dirname $file_path`/.`basename $file_path`.$TIME_ID_BACKUP"
-
 	fi
 	cp -v "$I/box$file_path" "$file_path"
 done
